b2835fb1f3d42334b18a5bb6812692093e8c3ced
lkprvt <leilaa.amirovaa@gmail.com>
2025-02-12 12:43:58 +0500

    Update signin.php

--- signin.php
+++ signin.php
@@ -2,19 +2,18 @@
 		if(!empty($_REQUEST['pass']) and !empty($_REQUEST['username']) and !empty($_REQUEST['lname']) and !empty($_REQUEST['number'])and !empty($_REQUEST['fname'])){
 
 		//fetch data from form input and save as variables	
-			$login=$_POST["username"];
-			$lname=$_POST["lname"];
-			$number=$_POST["number"];
-			$fname=$_POST["fname"];
-			$password=$_POST["pass"];
+			$login=$_POST["email"];
+			$username=$_POST["username"];
+			$password=$_POST["password"];
+			$conpassword=$_POST["conpassword"];
 
 		//connection with server
 			$link1=mysqli_connect("127.0.0.1", "root", ""); // save connection configurations as variable
 			mysqli_connect("127.0.0.1", "root", "") or die("Can't connect to server");//open connection with server 
-			mysqli_select_db($link1,"test") or die("Can't select to db");// select database from server
+			mysqli_select_db($link1,"tablename") or die("Can't select to db");// select database from server
 
 		//sql query
-			$queryvar="INSERT INTO `users`(`username`,`lname`,`fname`,`number`,`password`) VALUES ('$login','$lname','$fname', '$number', '$password')";
+			$queryvar="INSERT INTO `users`(`email`,`username`,`password`,`conpassword`) VALUES ('$login','$username','$password', '$conpassword')";
 
 		//check for query success
 			if(mysqli_query($link1,$queryvar)===TRUE){
